565b429ac1fdfc2d506a3058c7ba78f03835c6cd
great nnaji <nnajiuchegreat2004@gmail.com>
2026-05-29 12:19:35 -0400

    removed dead claim based logic

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -8,7 +8,6 @@ declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      role?: string | null;
       name?: string | null;
       email?: string | null;
       image?: string | null;
@@ -19,6 +18,5 @@ declare module "next-auth" {
 declare module "next-auth/jwt" {
   interface JWT {
     id?: string;
-    role?: string | null;
   }
 }
